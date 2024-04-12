d3f681fa60cbd18b8feebe2d371f54e40ea6cb1b
alashchev17 <andrew.lashchev15@gmail.com>
2024-04-12 14:33:55 +0200

    feat: added odometer to style balance counter

--- src/pages/mines/ui/Header.tsx
+++ src/pages/mines/ui/Header.tsx
@@ -1,13 +1,23 @@
-import { Heading } from "@/shared/Heading";
+import ReactOdometer from "react-odometerjs";
+
 import styles from "./Header.module.scss";
+
 import { useSettings } from "@/entities/Settings";
+import { Heading } from "@/shared/Heading";
 
 export const Header = () => {
   const { balance } = useSettings();
   return (
-    <div className={styles.container}>
+    <div className={styles["container"]}>
       <Heading text="Mines" level={6} />
-      <Heading text={`Balance: ${balance}`} level={6} />
+      <div className={styles["balance"]}>
+        <Heading text="Баланс:" level={6} />
+        <ReactOdometer
+          value={balance}
+          format="( ddd),dd"
+          style={{ fontWeight: 400, fontSize: "20px" }}
+        />
+      </div>
     </div>
   );
 };
